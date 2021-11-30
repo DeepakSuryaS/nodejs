f82595b740bf9b42d7211b96ddbc6bdcfb05523e
Deepak Surya <ideepaksuryas@gmail.com>
2021-11-30 22:06:00 +0530

    init node

---
+++ firstexpress.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "jade");
+app.get("/", function (req, res) {});
+
+const server = app.listen(3000, function () {});
