0983a8064391991f5d8890d4c7914c0562e09470
Deepak Surya <ideepaksuryas@gmail.com>
2021-12-20 23:56:13 +0530

    feat: first cli

---
+++ first-cli/index.js
@@ -0,0 +1,15 @@
+console.log(`
+Deepak Surya S
+
+  worker bees can leave
+  even drones can fly away
+  the queen is their slave
+
+  put a gun to my head and paint the walls with my brains
+
+  Twitter: ideepaksuryas
+  Github: DeepakSuryaS
+
+
+Have fun
+`);
