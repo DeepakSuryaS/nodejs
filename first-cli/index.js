4c40252a58b9bc4f9d2b4d8609ca4877e186c6d9
Deepak Surya <ideepaksuryas@gmail.com>
2022-01-04 08:42:51 +0530

    IMPROVE: add welcome header

--- first-cli/index.js
+++ first-cli/index.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 
-const clearConsole = require("clear-any-console");
+const welcome = require("cli-welcome");
+const pkgJson = require("./package.json");
 
-clearConsole();
+welcome({
+  title: pkgJson.name,
+  tagLine: "Howdy, nice to meet ya!",
+  description: pkgJson.description,
+  version: pkgJson.version,
+  bgColor: "#FADC00",
+  color: "#000",
+  bold: true,
+  clear: true,
+});
 
 console.log(`
 Deepak Surya S
