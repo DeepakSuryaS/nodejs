a9ecdb364081321e43a663237f908842107612f0
Deepak Surya S <ideepaksuryas@gmail.com>
2022-02-04 08:09:36 +0530

    improve: add colours

--- first-cli/index.js
+++ first-cli/index.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
-
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 const welcome = require("cli-welcome");
 const pkgJson = require("./package.json");
+import chalk from "chalk";
+const log = console.log;
+
+const twitter = chalk.hex("#1DA1F2").bold.inverse;
+const github = chalk.hex("#6CC644").bold.inverse;
 
 welcome({
-  title: pkgJson.name,
+  title: "Deepak Surya S",
   tagLine: "Howdy, nice to meet ya!",
   description: pkgJson.description,
   version: pkgJson.version,
@@ -14,18 +20,17 @@ welcome({
   clear: true,
 });
 
-console.log(`
-Deepak Surya S
-
+log(`${chalk.italic(`
   worker bees can leave
   even drones can fly away
   the queen is their slave
 
   put a gun to my head and paint the walls with my brains
+`)}
 
-  Twitter: ideepaksuryas
-  Github: DeepakSuryaS
+${twitter(" Twitter ")}: ideepaksuryas
+${github(" Github ")}: DeepakSuryaS
 
 
-Have fun
+${chalk.bgHex("#8E43E7")(" Have fun! ")}
 `);
